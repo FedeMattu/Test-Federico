8ba22ab856d02a16066aceaa527105dd6758454e
Federico Matturro <matturrofederico@gmail.com>
2025-11-14 12:18:28 +0100

    SubMenu working

--- testfederico/src/components/sections/SubMenu.jsx
+++ testfederico/src/components/sections/SubMenu.jsx
@@ -1,12 +1,55 @@
-import '../../App.css';
-import image1 from '../../assets/001.png';
+import "../../App.css";
+import search from "../../assets/search.svg";
+
+const categories = [
+  {
+    title: "Categoria di corso",
+  },
+  {
+    title: "Categoria di corso",
+  },
+  {
+    title: "Categoria di corso",
+  },
+  {
+    title: "Categoria di corso",
+  },
+  {
+    title: "Categoria di corso",
+  },
+  {
+    title: "Categoria di corso",
+  },
+];
 
 const SubMenu = () => {
   return (
-    <section className='subMenu'>
-      <h1 className='centeredImageText'>Main claim di impatto</h1>
-      <img className='imageSize' src={image1} alt="" />
-     
+    <section className="subMenu">
+      <div className="title">
+        <h1>Main claim di impatto</h1>
+
+        <form className="form">
+          <input
+            type="text"
+            placeholder="Search..."
+            name="search2"
+            className="searchInput"
+          ></input>
+          <button type="submit" className="searchButton">
+            <img className="searchIcon" src={search}></img>
+          </button>
+        </form>
+
+        <div className="categoryCollection">
+          {categories.map((item, index) => (
+            <div key={index}>
+              <div className="categotyButton">
+                <p>{item.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
