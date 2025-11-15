dcf54711246be0ff3ca672eaf42ba5c9f4f9af13
Federico Matturro <matturrofederico@gmail.com>
2025-11-15 13:45:58 +0100

    Slider Working

---
+++ testfederico/src/components/imageCarousel/ImageCarousel.jsx
@@ -0,0 +1,91 @@
+import "../styles/ImageCarousel.css";
+import image2 from "../../assets/002.png";
+import image3 from "../../assets/003.png";
+import image4 from "../../assets/004.png";
+import image5 from "../../assets/005.png";
+import arrowLeft from "../../assets/left.svg";
+import arrowRight from "../../assets/right.svg";
+import clock from "../../assets/timer.svg";
+import check from "../../assets/check_circle.svg";
+
+const cardItems = [
+  {
+    image: image2,
+    name: "Title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Scelerisque facilisis egestas arcu neque. Tincidunt vel eu duis fames volutpat in imperdiet enim nec.",
+    clock: "03:30h",
+    certificate: "Certificazione",
+  },
+  {
+    image: image3,
+    name: "Title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Scelerisque facilisis egestas arcu neque. Tincidunt vel eu duis fames volutpat in imperdiet enim nec.",
+    clock: "03:30h",
+    certificate: "Certificazione",
+  },
+  {
+    image: image4,
+    name: "Title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Scelerisque facilisis egestas arcu neque. Tincidunt vel eu duis fames volutpat in imperdiet enim nec.",
+    clock: "03:30h",
+    certificate: "Certificazione",
+  },
+  {
+    image: image5,
+    name: "Title",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Scelerisque facilisis egestas arcu neque. Tincidunt vel eu duis fames volutpat in imperdiet enim nec.",
+    clock: "03:30h",
+    certificate: "Certificazione",
+  },
+];
+
+const ImageCarousel = () => {
+  return (
+    <section className="sectionBeckground">
+      <h3>Titolo sezione</h3>
+      <div className="carousel" aria-label="Gallery">
+        <ol className="carousel__viewport">
+          {cardItems.map((item, index) => (
+            <li id="carousel__slide1" tabindex="0">
+              <div className="mycard" key={index}>
+                <div>
+                  <img className="myIMG" src={item.image} alt={item.name} />
+                  <div></div>
+                </div>
+                <div className="myCardText">
+                  <h4>{item.name}</h4>
+                  <p>{item.description}</p>
+                  <div className="finalInfo">
+                    <div className="iconText">
+                      <img className="searchIcon" src={clock}></img>
+                      <p>{item.clock}</p>
+                    </div>
+                    <div className="iconText">
+                      <img className="searchIcon" src={check}></img>
+                      <p>{item.certificate}</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ol>
+
+        <div className="arrowButtonsCollection">
+          <button className="carousel__prev">
+            <img className="searchIcon" src={arrowLeft}></img>
+          </button>
+          <button className="carousel__next">
+            <img className="searchIcon" src={arrowRight}></img>
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ImageCarousel;
--- testfederico/src/pages/Index.jsx
+++ testfederico/src/pages/Index.jsx
@@ -4,19 +4,21 @@ import SubMenu from "../components/sections/SubMenu";
 import Works from "../components/sections/Works";
 import Works2 from "../components/sections/Works2";
 import About from "../components/sections/About";
+import ImageCarousel from "../components/imageCarousel/ImageCarousel";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
-    <Navigation />
-        <main>
+      <Navigation />
+      <main>
         <SubMenu />
-            <Works />
-            <Works2 />
-            <About />
-        </main>
-        <hr className="separationLine" />
-    <Footer />
+        <ImageCarousel />
+        {/* <Works /> */}
+        <Works2 />
+        <About />
+      </main>
+      <hr className="separationLine" />
+      <Footer />
     </div>
   );
 };
