2bd1608f8e7f6bbaa707f94ce12a41e82ecca57e
Federico Matturro <matturrofederico@gmail.com>
2025-11-15 16:41:06 +0100

    Good point

--- testfederico/src/components/Footer.jsx
+++ testfederico/src/components/Footer.jsx
@@ -13,38 +13,46 @@ const Footer = () => {
             enim dolor in habitasse in senectus eleifend.
           </p>
         </div>
-        <div className="myFooterGrid">
-          <p>
-            <b>About</b>
-          </p>
-          <p>Home</p>
-          <p>Chi siamo</p>
-          <p>Cosa facciamo</p>
-          <p>Tutti i corsi</p>
-          <p>Contattaci</p>
-        </div>
-        <div className="myFooterGrid">
-          <p>
-            <b>Social</b>
-          </p>
-          <p>Facebook</p>
-          <p>Instagram</p>
-          <p>Linkedin</p>
-          <p>Youtube</p>
-        </div>
-        <div className="myFooterGrid">
-          <p>
-            <b>Supporto</b>
-          </p>
-          <p>Privacy</p>
-          <p>Cookies</p>
-          <p>Termini e condizioni</p>
-        </div>
-        <div className="myFooterGrid">
-          <p>
-            <b>Business</b>
-          </p>
-          <p>Eterna per le aziende</p>
+
+        <div className="footerTwo">
+          <div className="footerLayout">
+            <div className="myFooterGrid">
+              <p>
+                <b>About</b>
+              </p>
+              <p>Home</p>
+              <p>Chi siamo</p>
+              <p>Cosa facciamo</p>
+              <p>Tutti i corsi</p>
+              <p>Contattaci</p>
+            </div>
+            <div className="myFooterGrid">
+              <p>
+                <b>Social</b>
+              </p>
+              <p>Facebook</p>
+              <p>Instagram</p>
+              <p>Linkedin</p>
+              <p>Youtube</p>
+            </div>
+          </div>
+
+          <div className="footerLayout">
+            <div className="myFooterGrid">
+              <p>
+                <b>Supporto</b>
+              </p>
+              <p>Privacy</p>
+              <p>Cookies</p>
+              <p>Termini e condizioni</p>
+            </div>
+            <div className="myFooterGrid">
+              <p>
+                <b>Business</b>
+              </p>
+              <p>Eterna per le aziende</p>
+            </div>
+          </div>
         </div>
       </div>
     </section>
--- testfederico/src/pages/Index.jsx
+++ testfederico/src/pages/Index.jsx
@@ -1,8 +1,6 @@
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
 import SubMenu from "../components/sections/SubMenu";
-import Works from "../components/sections/Works";
-import Works2 from "../components/sections/Works2";
 import About from "../components/sections/About";
 import ImageCarousel from "../components/imageCarousel/ImageCarousel";
 import ImageCarousel2 from "../components/imageCarousel/ImageCarousel2";
@@ -15,8 +13,6 @@ const Index = () => {
         <SubMenu />
         <ImageCarousel />
         <ImageCarousel2 />
-        {/* <Works /> */}
-        {/* <Works2 /> */}
         <About />
       </main>
       <hr className="separationLine" />
